3b31ed4f1a584bf2b7d22636dc4c97aa0beb5f6c
Fynnyx <westerathf@gmail.com>
2021-10-08 16:03:17 +0200

    has_infos updated

--- app.py
+++ app.py
@@ -5,16 +5,18 @@ def has_infos(sites, points):
     sites_amount = len(sites)
     points_amount = len(points)
     print(sites_amount, points_amount)
-    if sites_amount == 2 and points_amount == 1:
+    if sites_amount == 3 and points_amount == 3:
         print("1")
-    elif sites_amount == 1 and points_amount == 2:
+    elif sites_amount == 2 and points_amount == 1:
         print("2")
-    elif sites_amount == 3 and points_amount == 0:
+    elif sites_amount == 1 and points_amount == 2:
         print("3")
-    elif sites_amount == 0 and points_amount == 3:
+    elif sites_amount == 3 and points_amount == 0:
         print("4")
-    else:
+    elif sites_amount == 0 and points_amount == 3:
         print("5")
+    else:
+        print("6")
 
 site_a = 13
 site_b = 28
@@ -26,11 +28,11 @@ point_c = 26
 sites = []
 points = []
 
-# sites.append(site_a)
+sites.append(site_a)
 sites.append(site_b)
 sites.append(site_c)
-# points.append(point_a)
-# points.append(point_b)
+points.append(point_a)
+points.append(point_b)
 points.append(point_c)
 
 print(sites, points)
