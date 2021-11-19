a611ec4a463cf523c10d89143f0f636766478eb4
nicomenzi <nicomenzii@gmail.com>
2021-11-19 16:14:33 +0100

    freitag

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, app, request, render_template, redirect, url_for
 
-
+app = Flask(__name__)
 def has_infos(sites, angles):
     print(sites, angles)
     sites_amount = len(sites)
@@ -27,6 +27,42 @@ angle_c = 26
 
 triangle = {"sites": {"a": 0, "b": 0, "c": 0}, "angles": {"a": 0, "b": 0, "c": 0}}
 
+
+@app.route("/")
+def home():
+    return render_template("html/index.html")
+
+
+@app.route("/form", methods=['POST'])
+def recive_form():
+
+    triangle = {"sites": {"a": request.form['site_a'], "b": request.form['site_b'], "c": request.form['site_c']}, "angles": {"a": request.form['angel_a'], "b": request.form['angel_b'], "c": request.form['angel_c']}}
+
+    if triangle['sites']["a"] != 0:
+        print("a!=0")
+
+    if triangle['sites']["b"] != 0:
+        print("b!=0")
+    if triangle['sites']["c"] != 0:
+        print("c!=0")
+
+    if triangle["angles"]["a"] != 0:
+        print("a!=0")
+    if triangle["angles"]["b"] != 0:
+        print("b!=0")
+    if triangle["angles"]["c"] != 0:
+        print("c!=0")
+
+    return redirect(url_for(home))
+
+
+
+
+
+
+
+
+
 triangle["sites"]["a"] = site_a
 triangle["sites"]["b"] = site_b
 triangle["sites"]["c"] = site_c
@@ -36,3 +72,6 @@ triangle["angles"]["b"] = angle_b
 triangle["angles"]["c"] = angle_c
 
 print(triangle)
+
+if __name__ == "__main__":
+    app.run()
